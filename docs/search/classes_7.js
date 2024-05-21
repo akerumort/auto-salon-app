1f4d608382c66b3e85a30cf0a684b4fa3310168e
akerumort <akerumort@gmail.com>
2024-05-21 14:37:23 +0300

    Changed docs, added SalesChartWindow.xaml.cs

--- docs/search/classes_7.js
+++ docs/search/classes_7.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['searchcarwindow_0',['SearchCarWindow',['../class_auto_salon_app_1_1_views_1_1_search_car_window.html',1,'AutoSalonApp::Views']]],
-  ['searchcarwindowcontroller_1',['SearchCarWindowController',['../class_auto_salon_app_1_1_controllers_1_1_search_car_window_controller.html',1,'AutoSalonApp::Controllers']]],
-  ['searchorderwindow_2',['SearchOrderWindow',['../class_auto_salon_app_1_1_views_1_1_search_order_window.html',1,'AutoSalonApp::Views']]],
-  ['searchorderwindowcontroller_3',['SearchOrderWindowController',['../class_auto_salon_app_1_1_controllers_1_1_search_order_window_controller.html',1,'AutoSalonApp::Controllers']]]
+  ['saleschartwindow_0',['SalesChartWindow',['../class_auto_salon_app_1_1_views_1_1_sales_chart_window.html',1,'AutoSalonApp::Views']]],
+  ['searchcarwindow_1',['SearchCarWindow',['../class_auto_salon_app_1_1_views_1_1_search_car_window.html',1,'AutoSalonApp::Views']]],
+  ['searchcarwindowcontroller_2',['SearchCarWindowController',['../class_auto_salon_app_1_1_controllers_1_1_search_car_window_controller.html',1,'AutoSalonApp::Controllers']]],
+  ['searchorderwindow_3',['SearchOrderWindow',['../class_auto_salon_app_1_1_views_1_1_search_order_window.html',1,'AutoSalonApp::Views']]],
+  ['searchorderwindowcontroller_4',['SearchOrderWindowController',['../class_auto_salon_app_1_1_controllers_1_1_search_order_window_controller.html',1,'AutoSalonApp::Controllers']]]
 ];
