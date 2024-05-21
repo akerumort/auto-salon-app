f1e71f495c0d4fb7a82211d6696807f7e25ccda5
akerumort <akerumort@gmail.com>
2024-05-21 14:28:39 +0300

    Sales analysis by year added to the menu, new window created

--- AutoSalonApp/Views/MainWindow.xaml.cs
+++ AutoSalonApp/Views/MainWindow.xaml.cs
@@ -19,7 +19,6 @@ public partial class MainWindow : Window
     private readonly MainWindowController _controller;
     private readonly SearchOrderWindowController _searchOrderController;
     private readonly SearchCarWindowController _searchCarController;
-    private readonly PieSeries pieSeries;
 
     /// <summary>
     /// Конструктор основного окна.
@@ -30,17 +29,6 @@ public partial class MainWindow : Window
         _controller = new MainWindowController();
         _searchOrderController = new SearchOrderWindowController();
         _searchCarController = new SearchCarWindowController();
-        
-        // Создание экземпляра PieSeries
-        pieSeries = new PieSeries
-        {
-            Title = "Проданные автомобили",
-            DependentValuePath = "Value",
-            IndependentValuePath = "Key"
-        };
-        
-        SalesChart.Series.Add(pieSeries);
-        UpdateSalesChart();
     }
 
     private void CreateDatabaseButton_Click(object sender, RoutedEventArgs e)
@@ -59,6 +47,12 @@ public partial class MainWindow : Window
 
     private void DeleteDatabaseButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.IsDatabaseCreated())
+        {
+            MessageBox.Show("Базы данных не существует, поэтому удаление невозможно.");
+            return;
+        }
+        
         MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите удалить базу данных?",
             "Подтверждение удаления", MessageBoxButton.YesNo, MessageBoxImage.Question);
         if (result == MessageBoxResult.Yes)
@@ -66,18 +60,12 @@ public partial class MainWindow : Window
             _controller.DeleteDatabase(); 
             OrdersDataGrid.ItemsSource = null;
             CarsDataGrid.ItemsSource = null;
-            SalesChart.Series.Clear();
         }
     }
     
     private void AddCarButton_Click(object sender, RoutedEventArgs e)
     {
-        if (!_controller.IsDatabaseCreated())
-        {
-            MessageBox.Show("Для добавления машины сначала нужно создать базу данных!");
-            return;
-        }
-        
+        if (!_controller.CheckDatabaseExistence()) return;
         AddCarWindow addCarWindow = new AddCarWindow(_controller.GetContext());
         addCarWindow.ShowDialog();
         RefreshCarsDataGrid();
@@ -85,23 +73,31 @@ public partial class MainWindow : Window
     
     private void PlaceOrderButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.CheckDatabaseExistence()) return;
         _controller.PlaceOrder();
         RefreshCarsDataGrid();
-        UpdateSalesChart();
     }
     
     private void EditCarButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.CheckDatabaseExistence()) return;
+
         // Проверяем, была ли выбрана строка в таблице
         if (CarsDataGrid.SelectedItem != null)
         {
-            Car selectedCar = (Car)CarsDataGrid.SelectedItem;
-            
-            EditCarWindow editCarWindow = new EditCarWindow(selectedCar, _controller.GetContext());
-            
-            editCarWindow.ShowDialog();
-            
-            RefreshCarsDataGrid();
+            // Проверяем тип выбранного объекта
+            if (CarsDataGrid.SelectedItem is Car selectedCar)
+            {
+                EditCarWindow editCarWindow = new EditCarWindow(selectedCar, _controller.GetContext());
+
+                editCarWindow.ShowDialog();
+
+                RefreshCarsDataGrid();
+            }
+            else
+            {
+                MessageBox.Show("Выберите корректную машину для редактирования.");
+            }
         }
         else
         {
@@ -111,19 +107,29 @@ public partial class MainWindow : Window
 
     private void EditOrderButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.CheckDatabaseExistence()) return;
+
+        // Проверяем, была ли выбрана строка в таблице
         if (OrdersDataGrid.SelectedItem != null)
         {
-            Order selectedOrder = (Order)OrdersDataGrid.SelectedItem;
-            EditOrderWindow editOrderWindow = new EditOrderWindow(selectedOrder, _controller.GetContext());
-
-            // Обработчик события сохранения изменений
-            editOrderWindow.OrderSaved += (o, args) =>
+            // Проверяем тип выбранного объекта
+            if (OrdersDataGrid.SelectedItem is Order selectedOrder)
             {
-                RefreshOrdersDataGrid(); 
-                RefreshCarsDataGrid(); 
-            };
-            
-            editOrderWindow.ShowDialog();
+                EditOrderWindow editOrderWindow = new EditOrderWindow(selectedOrder, _controller.GetContext());
+
+                // Обработчик события сохранения изменений
+                editOrderWindow.OrderSaved += (o, args) =>
+                {
+                    RefreshOrdersDataGrid(); 
+                    RefreshCarsDataGrid(); 
+                };
+
+                editOrderWindow.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Выберите корректный заказ для редактирования.");
+            }
         }
         else
         {
@@ -133,6 +139,7 @@ public partial class MainWindow : Window
     
     private void SearchButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.CheckDatabaseExistence()) return;
         // Создаем экземпляр окна поиска, передавая ссылку на главное окно
         SearchOrderWindow searchOrderWindow = new SearchOrderWindow(this);
         searchOrderWindow.SearchCompleted += SearchOrderWindow_SearchCompleted; // Добавляем обработчик события
@@ -141,6 +148,7 @@ public partial class MainWindow : Window
     
     private void SearchAllButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.CheckDatabaseExistence()) return;
         // Получаем текст для поиска из TextBox в MainWindow
         string searchText = SearchTextBox.Text;
 
@@ -160,6 +168,7 @@ public partial class MainWindow : Window
     
     private void SearchAllCarsButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.CheckDatabaseExistence()) return;
         // Получаем текст для поиска из TextBox в MainWindow
         string searchText = SearchCarTextBox.Text;
 
@@ -181,6 +190,7 @@ public partial class MainWindow : Window
     {
         try
         {
+            if (!_controller.CheckDatabaseExistence()) return;
             _controller.SaveDatabaseToJson();
         }
         catch (Exception ex)
@@ -191,6 +201,7 @@ public partial class MainWindow : Window
     
     private void SearchCarButton_Click(object sender, RoutedEventArgs e)
     {
+        if (!_controller.CheckDatabaseExistence()) return;
         // Создаем экземпляр окна расширенного поиска машин, передавая ссылку на главное окно
         SearchCarWindow searchCarWindow = new SearchCarWindow(this);
         searchCarWindow.SearchCompleted += SearchCarWindow_SearchCompleted; // Добавляем обработчик события
@@ -200,7 +211,6 @@ public partial class MainWindow : Window
     public void RefreshOrdersDataGrid()
     {
         OrdersDataGrid.ItemsSource = _controller.GetOrders();
-        UpdateSalesChart(); 
     }
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -225,22 +235,11 @@ public partial class MainWindow : Window
         // Обновляем DataGrid в MainWindow с использованием результатов поиска
         CarsDataGrid.ItemsSource = searchResults.ToList();
     }
-    private void UpdateSalesChart()
+    
+    private void OpenSalesChartWindowButton_Click(object sender, RoutedEventArgs e)
     {
-        List<Order> orders = _controller.GetOrders().ToList();
-        // Группируем заказы по годам оформления и считаем количество заказов в каждом году
-        var salesByYear = orders.GroupBy(order => order.OrderDate.Year)
-            .Select(group => new KeyValuePair<int, int>(group.Key, group.Count()));
-
-        SalesChart.Series.Clear();
-        ColumnSeries columnSeries = new ColumnSeries
-        {
-            Title = "Проданные автомобили",
-            ItemsSource = salesByYear,
-            DependentValuePath = "Value",
-            IndependentValuePath = "Key"
-        };
-
-        SalesChart.Series.Add(columnSeries);
+        if (!_controller.CheckDatabaseExistence()) return;
+        SalesChartWindow salesChartWindow = new SalesChartWindow(_controller);
+        salesChartWindow.Show();
     }
 }
---
+++ AutoSalonApp/Views/SalesChartWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using AutoSalonApp.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls.DataVisualization.Charting;
+using AutoSalonApp.Models;
+
+namespace AutoSalonApp.Views;
+
+/// <summary>
+/// Окно для отображения диаграммы продаж.
+/// </summary>
+public partial class SalesChartWindow : Window
+{
+    private readonly MainWindowController _controller;
+
+    /// <summary>
+    /// Конструктор окна диаграммы продаж.
+    /// </summary>
+    /// <param name="controller">Контроллер основного окна приложения.</param>
+    public SalesChartWindow(MainWindowController controller)
+    {
+        InitializeComponent();
+        _controller = controller;
+        UpdateSalesChart();
+    }
+
+    /// <summary>
+    /// Обновляет данные на диаграмме продаж.
+    /// </summary>
+    private void UpdateSalesChart()
+    {
+        List<Order> orders = _controller.GetOrders().ToList();
+        var salesByYear = orders.GroupBy(order => order.OrderDate.Year)
+            .Select(group => new KeyValuePair<int, int>(group.Key, group.Count()));
+
+        SalesChart.Series.Clear();
+        ColumnSeries columnSeries = new ColumnSeries
+        {
+            Title = "Проданные автомобили",
+            ItemsSource = salesByYear,
+            DependentValuePath = "Value",
+            IndependentValuePath = "Key"
+        };
+
+        SalesChart.Series.Add(columnSeries);
+    }
+}
